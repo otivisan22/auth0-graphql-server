6ff21ebf7711462b5e7e84b1b345010b110678e1
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-10-22 23:38:37 +0100

    set up server/schema/resolvers

--- src/index.js
+++ src/index.js
@@ -2,3 +2,24 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const resolvers = {
+  hello: () => "Hello world!",
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+  })
+);
+app.listen(4000);
+
+console.log(`🚀 Server ready at http://localhost:4000/graphql`);
