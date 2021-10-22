544a839bd537ababdb8288f86d26f20557672920
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-10-23 00:06:55 +0100

    define schema

---
+++ src/schema.js
@@ -0,0 +1,11 @@
+const { buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+module.exports = schema;
