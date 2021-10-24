7faed183d1973b24a48b5a6e672f6248b14edfd5
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-10-24 23:16:01 +0100

    modified schema for testing the server

--- src/schema.js
+++ src/schema.js
@@ -1,11 +1,25 @@
-const { buildSchema } = require("graphql");
+const { buildSchema, GraphQLBoolean } = require("graphql");
 
 // Construct a schema, using GraphQL schema language
-
-const schema = buildSchema(`
-  type Query {
-    hello: String
+const schema = gql`
+  type Event {
+    id: ID!
+    title: String!
+    description: String
+    date: String
+    attendants: [Person!]
   }
-`);
+
+  type Person {
+    id: ID!
+    name: String!
+    age: Int
+  }
+
+  type Query {
+    events: [Event!]!
+    event(id: Int!): Event!
+  }
+`;
 
 module.exports = schema;
