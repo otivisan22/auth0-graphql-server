65fc020353346e848435c10d4544d0b3e89a5745
otivisan22 <71434012+otivisan22@users.noreply.github.com>
2021-12-15 23:25:16 +0000

    modified the content of the server

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
-// Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-// The root provides a resolver function for each API endpoint
 const root = {
   hello: () => {
     return "Hello world!";
